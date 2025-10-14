7e563f12c0e1eed34df3e8ed89c693148b4952b8
爽歪歪 <2821423807@qq.com>
2025-10-14 14:27:00 +0800

    完成1249

---
+++ 栈/1249.移除无效的括号.cpp
@@ -0,0 +1,32 @@
+#include<LeetCodeStructs.h>
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        string res = "";
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                st.push(res.size());
+                res.push_back(s[i]);
+            }
+            else if(s[i] == ')'){
+                if(!st.empty()){
+                    st.pop();
+                    res.push_back(s[i]);
+                }
+                else{
+                    continue;
+                }
+            }
+            else{
+                res.push_back(s[i]);
+            }
+        }
+
+        while(!st.empty()){
+            res.erase(res.begin() + st.top());
+            st.pop();
+        }
+        return res;
+    }
+};
