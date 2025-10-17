f988f3b07a9ed287145a1462dfc2cf1271ada091
爽歪歪 <2821423807@qq.com>
2025-10-17 12:58:02 +0800

    完成239

--- 队列/239.滑动窗口最大值.cpp
+++ 队列/239.滑动窗口最大值.cpp
@@ -2,7 +2,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> q(k);
+        vector<int> res(n - k + 1);
+        int index;
+        for(int i = 0; i < nums.size(); i++){
+            index = i - k + 1;
+            if(index < 0){
+                while(!q.empty() && nums[q.back()] < nums[i]){
+                    q.pop_back();
+                }
+                q.push_back(i);
+            }
+            else{
+                while(!q.empty() && (nums[q.back()] < nums[i] || q.back() < index)){
+                    q.pop_back();
+                }
+                q.push_back(i);
 
+                while(q.front() < index){
+                    q.pop_front();
+                }
+                res[index] = nums[q.front()];
+            }
+        }
+        return res;
     }
 };
 
