9b5b97a487f7942e0864e2252b6e6e6ee0c4643e
爽歪歪 <2821423807@qq.com>
2025-10-21 20:48:39 +0800

    完成73

---
+++ 数组/73.矩阵置零.cpp
@@ -0,0 +1,25 @@
+#include<LeetCodeStructs.h>
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool *rows = new bool[m]();
+        bool *cols = new bool[n]();
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(rows[i] || cols[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
