a9689b59fab190398cdafea76a5b64d8ce917cad
爽歪歪 <2821423807@qq.com>
2025-11-12 11:55:02 +0800

    更正最大深度递归写法

--- 二叉树/104.二叉树的最大深度.cpp
+++ 二叉树/104.二叉树的最大深度.cpp
@@ -5,8 +5,13 @@ public:
         if(!root){
             return 0;
         }
+        if(!root->left && !root->right){
+            return 1;
+        }
         else{
-            return max(maxDepth(root->left),maxDepth(root->right)) + 1;
+            int LMax = root->left ? maxDepth(root->left) : INT_MIN;
+            int RMax = root->right ? maxDepth(root->right) : INT_MIN;
+            return max(LMax,RMax) + 1;
         }
     }
 };
