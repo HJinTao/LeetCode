7003d7d04d135a8f60960a9dcfa0d97697405ec2
爽歪歪 <2821423807@qq.com>
2026-07-08 20:53:07 +0800

    添加并实现并查集（DSU）数据结构

--- LeetCodeStructs.h
+++ LeetCodeStructs.h
@@ -29,3 +29,44 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *l, TreeNode *r) : val(x), left(l), right(r) {}
 };
+
+struct DSU{
+    vector<int> parent;
+    vector<int> rank;
+    DSU(int n){
+        parent = vector<int>(n);
+        iota(parent.begin(), parent.end(), 0);
+        rank = vector<int>(n, 1);
+    }
+
+    int find(int x){
+        if(x != parent[x]){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    bool isConnected(int x, int y){
+        return find(x) == find(y);
+    }
+
+    void unite(int x, int y){
+        int rootX = find(x), rootY = find(y);
+        if(rootX == rootY){
+            return;
+        }
+        else{
+            int rx = rank[rootX], ry = rank[rootY];
+            if(rx < ry){
+                parent[rootX] = rootY;
+            }
+            else if(rx == ry){
+                parent[rootX] = rootY;
+                rank[rootY]++;
+            }
+            else{
+                parent[rootY] = rootX;
+            }
+        }
+    }
+};
